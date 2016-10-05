315fd7286e57a7f623e9e67693ae81ae242279b3
Varsity Software <github@varsitysoftware.com>
2016-10-05 11:56:28 -0400

    Update GPUVideo.m

--- src/ios/GPUVideo.m
+++ src/ios/GPUVideo.m
@@ -8,3 +8,22 @@
 #import "GPUVideo.h"
 #import <Accounts/Accounts.h>
 
+@implementation GPUVideo 
+
+@synthesize callbackId;
+
+- (void) playVideo:(CDVInvokedUrlCommand *)command {
+    
+    NSDictionary *options = [command.arguments objectAtIndex: 0];
+  
+	NSString * strFileURL = [options objectForKey:@"fileURL"];
+	
+    self.callbackId = command.callbackId;
+
+	NSLog(@"GPUVIDEO BABY!!!!");    
+
+	CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+	[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];	
+    
+}
+@end
