f1673e7d48e4a56cb728adf7766968d47674df0e
Varsity Software <github@varsitysoftware.com>
2016-10-10 14:05:42 -0400

    Update GPUVideo.h

--- src/ios/GPUVideo.h
+++ src/ios/GPUVideo.h
@@ -7,11 +7,99 @@
 //
 
 #import <Cordova/CDVPlugin.h>
+#import <GPUImage/GPUImageFramework.h>
+#import <GPUImage/GPUImageContext.h>
+#import <GPUImage/GPUImageOutput.h>
+
+
+/** Protocol for getting Movie played callback.
+*/
+@protocol GPUVideoMovieDelegate <NSObject>
+
+-(void)didCompletePlayingMovie;
+@end
+
+/** Source object for filtering movies
+*/
+@interface GPUVideoMovie : GPUImageOutput
+
+@property (readwrite, retain) AVAsset *asset;
+@property (readwrite, retain) AVPlayerItem *playerItem;
+@property(readwrite, retain) NSURL *url;
+
+@property(readwrite, nonatomic) BOOL pause;
+@property(readwrite, nonatomic) BOOL stop;
+@property(readwrite, nonatomic) float currentTime;
+@property(readwrite, nonatomic) int currentTimeInSecs;
+@property(readwrite, nonatomic) float duration;
+
+/** This enables the benchmarking mode, which logs out instantaneous and average frame times to the console
+*/
+@property(readwrite, nonatomic) BOOL runBenchmark;
+
+/** This determines whether to play back a movie as fast as the frames can be processed, or if the original speed of the movie should be respected. Defaults to NO.
+*/
+@property(readwrite, nonatomic) BOOL playAtActualSpeed;
+
+/** This determines whether the video should repeat (loop) at the end and restart from the beginning. Defaults to NO.
+*/
+@property(readwrite, nonatomic) BOOL shouldRepeat;
+
+/** This specifies the progress of the process on a scale from 0 to 1.0. A value of 0 means the process has not yet begun, A value of 1.0 means the conversaion is complete.
+This property is not key-value observable.
+*/
+@property(readonly, nonatomic) float progress;
+
+/** This is used to send the delete Movie did complete playing alert
+*/
+@property (readwrite, nonatomic, assign) id <GPUImageMovieDelegate>delegate;
+
+@property (readonly, nonatomic) AVAssetReader *assetReader;
+@property (readonly, nonatomic) BOOL audioEncodingIsFinished;
+@property (readonly, nonatomic) BOOL videoEncodingIsFinished;
+
+/// @name Initialization and teardown
+-(id)initWithAsset:(AVAsset *)asset;
+-(id)initWithPlayerItem:(AVPlayerItem *)playerItem;
+-(id)initWithURL:(NSURL *)url;
+-(void)yuvConversionSetup;
+
+/// @name Movie processing
+-(void)enableSynchronizedEncodingUsingMovieWriter:(GPUImageMovieWriter *)movieWriter;
+-(BOOL)readNextVideoFrameFromOutput:(AVAssetReaderOutput *)readerVideoTrackOutput;
+-(BOOL)readNextAudioSampleFromOutput:(AVAssetReaderOutput *)readerAudioTrackOutput;
+-(void)startProcessing;
+-(void)endProcessing;
+-(void)cancelProcessing;
+-(void)processMovieFrame:(CMSampleBufferRef)movieSampleBuffer;
+-(void)pauseVideo:(CDVInvokedUrlCommand *)command;
+@end
+
 
 @interface GPUVideo : CDVPlugin < UINavigationControllerDelegate, UIScrollViewDelegate>
+{
+	//GPUImageMovie *movieFile;
+	GPUVideoMovie *movieFile;
+	GPUImageOutput<GPUImageInput> *filter;
+	GPUImageOutput<GPUImageInput> *filter2;
+	GPUImageMovieWriter *movieWriter;
+	NSTimer * timer;
+}
 
-@property (copy)   NSString* callbackId;
+@property (copy)NSString* callbackId;
 
-- (void)playVideo:(CDVInvokedUrlCommand *)command;
+@property (nonatomic, strong)AVAudioPlayer *player;
+@property (strong, nonatomic) UIView* rootView;
+@property (strong, nonatomic) UIView* videoView;
+@property (readwrite, nonatomic) CDVPluginResult* pluginResult;
+@property (readwrite, nonatomic) NSMutableDictionary* jsonResults;
+
+//NSMutableDictionary *jsonResults = [[NSMutableDictionary alloc] init];
+
+
+-(void)startVideo:(CDVInvokedUrlCommand *)command;
+-(void)playVideo:(CDVInvokedUrlCommand *)command;
+-(void)pauseVideo:(CDVInvokedUrlCommand *)command;
+-(void)stopVideo:(CDVInvokedUrlCommand *)command;
 
 @end
